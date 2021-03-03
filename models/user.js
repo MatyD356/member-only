9748c2a36d18783fdb7403a340df077ab8608625
Mateusz <matyd356@gmail.com>
2021-03-03 20:05:49 +0100

    add userSchema

---
+++ models/user.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema
+
+const UserSchema = new Schema({
+  firstName: { type: String, required: true, maxlength: 100 },
+  lastName: { type: String, required: true, maxlength: 100 },
+  email: { type: String, required: true, maxlength: 100 },
+  password: { type: String, required: true, maxlength: 300 },
+})
+
+module.exports = mongoose.model('User', UserSchema)
