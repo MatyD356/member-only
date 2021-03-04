91f4321e87b18a25702ee398cc1bab1244f86a73
Mateusz <matyd356@gmail.com>
2021-03-04 19:10:40 +0100

    add usercontroller

---
+++ controller/userController.js
@@ -0,0 +1,6 @@
+
+
+//display sing-up new user form
+exports.add_user = (req, res) => {
+  res.render('sing-up', { title: 'howdy' })
+}
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+const user_Controller = require('../controller/userController')
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+//GET sing-up form
+router.get('/sing-up', user_Controller.add_user);
 
 module.exports = router;
